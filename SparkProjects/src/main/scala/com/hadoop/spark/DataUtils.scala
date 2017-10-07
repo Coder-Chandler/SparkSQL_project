aacc50d7b2ade47394a3d02ffdce79efd5e34e1c
Chandler <ysiwgtus@gmail.com>
2017-10-07 21:48:31 +0800

    Spark SQL project
    Spark SQL project

---
+++ SparkProjects/src/main/scala/com/hadoop/spark/DataUtils.scala
@@ -0,0 +1,39 @@
+package com.hadoop.spark
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.Date
+
+
+/**
+  * 日期时间解析工具类
+  */
+object DataUtils {
+  //输入文件日期时间格式
+  //[30/May/2013:17:38:20 +0800]
+  val YYYYMMDDHHMM_TIME_FORMAT = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z", Locale.ENGLISH)
+
+  //转换输入文件日期格式
+  val TAGET_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+  /**
+    *获取时间：yyyy-MM-dd HH:mm:ss
+    */
+  def parse(time:String) = {
+    TAGET_FORMAT.format(new Date(getTime(time)))
+  }
+
+  /**
+    * 获取输入日志时间：long类型
+    */
+  def getTime(time:String) = {
+    try {
+      YYYYMMDDHHMM_TIME_FORMAT.parse(time.substring(time.indexOf("[") + 1,
+        time.lastIndexOf("]"))).getTime
+    } catch {
+      case e: Exception => {
+        0l
+      }
+    }
+  }
+}
--- SparkProjects/src/main/scala/com/hadoop/spark/SparkStatFormat.scala
+++ SparkProjects/src/main/scala/com/hadoop/spark/SparkStatFormat.scala
@@ -14,7 +14,19 @@ object SparkStatFormat {
 
     val access = spark.sparkContext.textFile("file:///Users/chandler/Documents/data/test_data/access_2013_05_30.log")
 
-    access.take(20).foreach(println)
+    //access.take(20).foreach(println)
+
+    access.map(line => {
+      val splits = line.split(" ")
+      val ip = splits(0)
+
+      /**
+        *原始日志的第三个和第四个字段拼接就是完整的访问时间：
+        * [30/May/2013:17:38:20 +0800] ==> yyyy-MM-dd HH:mm:ss
+        */
+      val time = splits(3)+" "+splits(4)
+      (ip, DataUtils.parse(time))
+    }).take(10).foreach(println)
 
     spark.stop()
   }
